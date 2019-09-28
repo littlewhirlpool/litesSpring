95d02d139909cfe3c73154617e60ca9c3ba2483a
weizhenfang <1837192773@qq.com>
2019-09-28 22:19:37 +0800

    add to version

---
+++ src/main/java/org/litespring/beans/factory/BeanCreationException.java
@@ -0,0 +1,40 @@
+package org.litespring.beans.factory;
+
+import org.litespring.beans.BeansException;
+
+import java.util.Scanner;
+
+/**
+ * @program: litespring->BeanCreationException
+ * @description: 创建bean时抛出的异常
+ * @author: weizhenfang
+ * @create: 2019-09-26 08:43
+ **/
+public class BeanCreationException extends BeansException {
+
+    private String beanName;
+
+    public BeanCreationException(String msg) {
+        super(msg);
+    }
+
+    public BeanCreationException(String msg , Throwable cause){
+        super(msg , cause);
+    }
+
+    public BeanCreationException(String beanName , String msg){
+        super("Error creating bean with name '" + beanName + "':" + msg);
+        this.beanName = beanName;
+    }
+
+    public BeanCreationException(String beanName , String msg , Throwable cause){
+        this(beanName,msg);
+        initCause(cause);
+    }
+
+    public String getBeanName(){
+        return this.beanName;
+    }
+
+
+}
